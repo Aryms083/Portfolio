e23c95b0e8a6a13fd307f83286b5dc55ed892756
Aryan Maheshwari <114201239+Aryms083@users.noreply.github.com>
2025-06-10 23:08:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/* script.js */
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".btn");
+    
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            alert(`You clicked on ${this.innerText}!`);
+        });
+    });
+});
